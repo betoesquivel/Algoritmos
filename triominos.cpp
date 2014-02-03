748145373de5316b70578fe10f6171075ba6d081
Beto Esquivel <beto.esquivel.p@gmail.com>
2014-02-03 16:53:36 -0600

    triominos first commit

---
+++ triominos.cpp
@@ -0,0 +1,29 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  triominos.cpp
+ *
+ *    Description:  Problem from Algorythms class. Given a nxn (where n is power of 2
+ *    sized) matrix) and a cell (r,c) in matrix. Paint the whole matrix writing only
+ *    triominios (L shapes using 3 cells).
+ *
+ *        Version:  1.0
+ *        Created:  02/03/2014 16:50:50
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Jose Alberto Esquivel Patino (), 
+ *   Organization:  ITESM Campus Monterrey
+ *
+ * =====================================================================================
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char *argv[])
+{
+	
+	return 0;
+}
