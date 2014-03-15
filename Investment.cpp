2d00113e08d5585ae5bfd9a388cbfda5482ee595
ppesq <betoesquivel>
2014-03-14 18:41:21 -0600

    Creo que ya funciona Invest

--- Investment.cpp
+++ Investment.cpp
@@ -10,7 +10,8 @@
  *       Revision:  none
  *       Compiler:  gcc
  *
- *         Author:  Jose Alberto Esquivel Patino (), 
+ *         Author:  Jose Alberto Esquivel Patino A01139626
+ *         Author2: Hugo Leon Garza A01139720
  *   Organization:  ITESM Campus Monterrey
  *
  * =====================================================================================
@@ -21,14 +22,26 @@ using namespace std;
 
 int pricePerDay[100];//contains the price of the object to sell every day
 int days; 
+bool debug = false;
 
 //returns which of the 3 numbers is greater
 int greaterNum (int n1, int n2, int n3) {
+	int greaterIndex = 1; 
 	int g = n1; 
-	g = ( n2 > g ) ? n2:g; 
-	g = ( n3 > g ) ? n3:g; 
+	if (n2 > n1) {
+		g = n2; 
+		greaterIndex = 2; 
+	}
+	if (n3 > g) {
+		g = n3;
+		greaterIndex = 3; 
+	}
 
-	return g; 
+	if (debug) {
+		cout<<"Calcule el mayor de: "<<n1<<' '<<n2<<' '<<n3<<endl;
+		cout<<"Mayor = "<<g<<' '<<greaterIndex<<endl;
+	}
+	return greaterIndex; 
 }
 
 //inf y sup son los indices inferior y superior del arreglo 
@@ -38,14 +51,14 @@ void bestInvestment(int inf, int sup, int *min, int *max) {
 	int min1, max1, min2, max2; //buying and selling day from both halves
 	if (size > 2) {
 		int pivot = (inf + sup) / 2; 
-		bestInvestment (inf, pivot, &min1, &max1); 
-		bestInvestment (pivot+1, sup, &min2, &max2); 
+		bestInvestment (inf, pivot-1, &min1, &max1); 
+		bestInvestment (pivot, sup, &min2, &max2); 
 
 		//modifico las variables min y max (son mis valores de retorno)
 		int dif1, dif2, dif3; 
 		dif1 = pricePerDay[max1] - pricePerDay[min1]; 
 		dif2 = pricePerDay[max2] - pricePerDay[min2]; 
-		dif3 = pricePerDay[max1] - pricePerDay[min2]; 
+		dif3 = pricePerDay[min2] - pricePerDay[max1]; 
 		int greaterDif = greaterNum(dif1, dif2, dif3);
 		switch (greaterDif){
 			case 1: 				
@@ -106,6 +119,7 @@ int main(int argc, const char *argv[])
 
 		//obtengo buyingDay y sellingDay enviandolos como ref
 		bestInvestment (inf, sup, &buyingDay, &sellingDay); 
+		buyingDay++; sellingDay++;
 		cout<< "Buy in " << buyingDay << ", sell in " << sellingDay << endl;
 		
 		count++;		
