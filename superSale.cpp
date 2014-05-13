9c9f59cb932f4fc5f7d4ed7430c14a6f3ba85e05
Beto Esquivel <ppbeto94@gmail.com>
2014-05-12 19:20:23 -0500

    got it

--- superSale.cpp
+++ superSale.cpp
@@ -4,6 +4,8 @@
 
 using namespace std;
 
+bool debug = false;
+
 void requestObjects (int prices[], int weights[], int numObjects){
     for (int i = 0; i < numObjects; i++){
         cin >> prices[i] >> weights[i];
@@ -51,25 +53,72 @@ int dynamicBackpack(int prices[], int weights[], int nObjects, int people[], int
         orderedPeople.push(people[i]);
     }
 
-    cout << "Ordered: " << endl;
-    while (!orderedPeople.empty()){
-        cout << orderedPeople.top() << endl;
-        orderedPeople.pop();
+    if (debug){
+        cout << "Ordered: " << endl;
+        while (!orderedPeople.empty()){
+            cout << orderedPeople.top() << endl;
+            orderedPeople.pop();
+        }
     }
 
+    int currentObj, spaceLeft;
     //dynamic programming solution
     //r starts in 1 since the first row is going to stay
     //filled with 0s
-    for (int r = 1; r < nObjects; r++){
+    for (int r = 1; r <= nObjects; r++){
+        currentObj = r - 1; // object in my parallel arrays...
+        if (debug){
+            cout << "Filling for obj: " << currentObj << endl;
+        }
 
+        for (int c = 1; c <= maxPersonCapacity; c++){
+            if (debug){
+                cout << "Solving subproblem... Kanpsack weight: " << c << endl;
+            }
+            //calculate space left in knapsack if I include
+            //the current obj
+            spaceLeft = c - weights[currentObj];
 
-        for (int c = 0; c < maxPersonCapacity; c++){
+            //if current object fits
+            if ( spaceLeft >= 0 ){
+                if (debug){
+                    cout << "Obj fits, space left: " << spaceLeft << endl;
+                }
 
+                //current object does fit
+                D[r][c] = getMax(D[r-1][c], prices[currentObj] + D[r-1][spaceLeft]);
+                if (debug){
+                    cout << "Max possible value: " << D[r][c] << endl;
+                }
+
+            }else{
+
+                //current object does not fit
+                //inherit value above
+                D[r][c] = D[r-1][c];
+                if (debug){
+                    cout << "Doesnt fit, so: " << D[r][c] << endl;
+                }
+
+            }
         }
 
 
     }
 
+    if (debug){
+        cout << "Matrix after all operations: " << endl;
+        for (int r = 0; r <= nObjects; r++){
+            for (int c = 0; c <= maxPersonCapacity; c++ ){
+                cout << D[r][c] << ' ';
+            }
+            cout << endl;
+        }
+    }
+
+    for (int i = 0; i < nPeople; i++){
+        max += D[nObjects][people[i]];
+    }
     return max;
 }
 
@@ -91,7 +140,9 @@ int main(int argc, char const* argv[])
 
         //get objects from standard input
         requestObjects(prices, weights, nObjects);
-        printObjects(prices, weights, nObjects);
+        if (debug){
+            printObjects(prices, weights, nObjects);
+        }
 
         cin >> nPeople;
 
@@ -100,12 +151,18 @@ int main(int argc, char const* argv[])
 
         //get maximum weight that each person can carry from stdin
         int maxCapacity = requestWeights(people, nPeople);
-        printWeights(people, nPeople);
+        if (debug){
+            printWeights(people, nPeople);
+        }
 
         //get the maximum value of goods I can get with this group
         maxValue = dynamicBackpack(prices, weights, nObjects, people, nPeople, maxCapacity);
 
-        cout << "This is the max value your group can carry: " << maxValue << endl;
+        if (debug){
+            cout << "This is the max value your group can carry: ";
+        }
+
+        cout << maxValue << endl;
 
         //increase case counter
         cont++;
